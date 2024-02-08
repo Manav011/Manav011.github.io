a48ccd82a44c4117a3baa6cbb34a3ed1f9762412
Manav <manavbafna87+Github@gmail.com>
2024-02-08 18:27:23 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.scss';
 import { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import Head from 'next/head';
 import Script from 'next/script';
 
 const inter = Inter({ 
@@ -26,13 +25,11 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <Head>
       <Script
           defer
           data-domain="manav.is-a.dev"
           src="https://analytics.is-a.dev/js/script.js"
         />
-      </Head>
       <body  className={`${inter.className} h-[640vh] lg:h-[700vh] relative pt-28 sm:pt-36 bg-gray-900 text-gray-50 text-opacity-90`}>
         <div className=" fixed top-[6rem] -z-10 right-[11rem] h-[25rem] w-[25rem] rounded-full blur-[10rem] sm:w-[68.75rem] bg-[#946263]"></div>
         <div className=" fixed top-[1rem] -z-10 left-[11rem] h-[25rem] w-[25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] bg-[#676394]"></div>
